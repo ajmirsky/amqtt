de66e17c8ae79feb274dd7dd4a478deb8c88b3dd
Andrew Mirsky <andrew@mirsky.net>
2025-07-02 16:23:40 -0400

    updating load test script

--- samples/load_test.py
+++ samples/load_test.py
@@ -1,4 +1,5 @@
 import asyncio
+from asyncio import CancelledError
 from random import randint
 
 from amqtt.client import MQTTClient
@@ -14,32 +15,45 @@ async def run_client() -> None:
         ('$SYS/#', QOS_0),
         ('a/b', QOS_0),
     ])
-    msg_count = randint(10, 100)
-    for _ in range(msg_count):
-        msg = lorem.words(randint(2, 10))
-        await client.publish("a/b", msg.encode())
-        print("sending message")
+    try:
+        msg_count = randint(10, 100)
+        for _ in range(msg_count):
+            msg = lorem.words(randint(2, 10))
+            await client.publish("a/b", msg.encode())
+            print("sending message")
 
-        rcv_count = 0
-        try:
-            while True:
-                msg = await client.deliver_message(timeout_duration=0.5)
-                rcv_count += 1
-        except asyncio.TimeoutError:
-            print(f"received {rcv_count} messages")
+            rcv_count = 0
+            try:
+                while True:
+                    msg = await client.deliver_message(timeout_duration=0.5)
+                    rcv_count += 1
+            except asyncio.TimeoutError:
+                print(f"received {rcv_count} messages")
 
 
-        await asyncio.sleep(randint(1, 3))
+            await asyncio.sleep(randint(1, 3))
+    except CancelledError as e:
+        await client.disconnect()
+        print("client cancelled")
+        raise
 
     await client.disconnect()
     print("client exiting")
 
 async def main() -> None:
+    tasks = []
+    try:
+        while True:
+            print("launching new client")
+            tasks.append(asyncio.create_task(run_client()))
+            await asyncio.sleep(0.25)
+    except KeyboardInterrupt:
+        for task in tasks:
+            if not task.done():
+                task.cancel()
 
-    while True:
-        print("launching new client")
-        asyncio.ensure_future(run_client())
-        await asyncio.sleep(0.25)
+
+
 
 
 if __name__ == "__main__":
