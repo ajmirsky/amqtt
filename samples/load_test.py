3d421016465aec9565e68558f2335e72185974fe
Andrew Mirsky <andrew@mirsky.net>
2025-07-15 17:01:10 -0400

    updated load testing script

--- samples/load_test.py
+++ samples/load_test.py
@@ -1,5 +1,7 @@
 import asyncio
+import time
 from asyncio import CancelledError
+from math import floor
 from random import randint
 
 from amqtt.client import MQTTClient
@@ -8,45 +10,83 @@ from lorem_text import lorem
 from amqtt.mqtt.constants import QOS_0
 
 
-async def run_client() -> None:
+class Counter:
+    def __init__(self):
+        self.running_clients = 0
+        self.messages_sent = 0
+        self.max_clients = randint(20, 40)
+
+
+def get_topic():
+
+    t = ['a', 'b', 'c']
+    q = ['a/b', 'c/d', 'e/f', 'g/h']
+
+    # return f"{t[randint(0, len(t) - 1)]}/{t[randint(0, len(t) - 1)]}"
+    return f"{q[randint(0, len(t) - 1)]}"
+
+
+async def run_client(counter: Counter) -> None:
+    start_time = time.time()
+    counter.running_clients += 1
+    topic = get_topic()
     client = MQTTClient()
-    await client.connect("mqtt://localhost:1883/")
+    await client.connect("mqtt://test.amqtt.io:1883/")
     await client.subscribe([
         # ('$SYS/#', QOS_0),
-        ('a/b', QOS_0),
+        (topic, QOS_0),
     ])
     try:
         msg_count = randint(10, 100)
         for _ in range(msg_count):
             msg = lorem.words(randint(2, 10))
-            await client.publish("a/b", msg.encode())
-            print("sending message")
+            await client.publish(topic, msg.encode())
+            await asyncio.sleep(randint(1, 10)/30)
+            # print("sending message")
 
             rcv_count = 0
             try:
                 while True:
-                    msg = await client.deliver_message(timeout_duration=0.5)
+                    _ = await client.deliver_message(timeout_duration=0.5)
                     rcv_count += 1
             except asyncio.TimeoutError:
-                print(f"received {rcv_count} messages")
+                # print(f"received {rcv_count} messages")
+                pass
 
+            # await asyncio.sleep(randint(1, 3))
 
-            await asyncio.sleep(randint(1, 3))
     except CancelledError as e:
         await client.disconnect()
         print("client cancelled")
         raise
 
     await client.disconnect()
-    print("client exiting")
+    print(f"client exiting after {floor(time.time() - start_time)} seconds")
+    counter.running_clients -= 1
+
+async def running_client_max(counter:Counter) -> None:
+    while True:
+        if counter.max_clients < 60:
+            counter.max_clients += randint(-10, 20)
+        elif counter.max_clients > 60:
+            counter.max_clients += randint(-20, 10)
+        elif counter.max_clients < 0:
+            counter.max_clients = randint(20, 40)
+
+        print(f"max clients now >>>>>> {counter.max_clients}")
+        await asyncio.sleep(45)
+
 
 async def main() -> None:
     tasks = []
+    counter = Counter()
+    tasks.append(asyncio.create_task(running_client_max(counter)))
     try:
         while True:
-            print("launching new client")
-            tasks.append(asyncio.create_task(run_client()))
-            await asyncio.sleep(0.5)
+            if counter.running_clients < counter.max_clients:
+                print(f"launching new client: current running {counter.running_clients}")
+                tasks.append(asyncio.create_task(run_client(counter)))
+            await asyncio.sleep(0.25)
     except KeyboardInterrupt:
         for task in tasks:
             if not task.done():
